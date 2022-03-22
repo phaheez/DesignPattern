f3ea9001ff53d35301d61e67758759100d45a83f
phaheez <faizfasasi@gmail.com>
2022-03-22 15:36:27 +0100

    Singleton Implementation

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = SingletonDataContainer.GetInstance();
+            var s2 = SingletonDataContainer.GetInstance();
+
+            if (s1.Equals(s2))
+            {
+                Console.WriteLine("Singleton instance is equal");
+                var name = s1.GetCustomerName("John Doe");
+                Console.WriteLine(name);
+            }
+            else
+            {
+                Console.WriteLine("Singleton instance is not equal");
+            }
+        }
+    }
+}
---
+++ SingletonPattern/SingletonDataContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public sealed class SingletonDataContainer
+    {
+        private SingletonDataContainer() { }
+
+        private static Lazy<SingletonDataContainer> _instance = null;
+
+        public static SingletonDataContainer GetInstance()
+        {
+            if (_instance is null)
+            {
+                _instance = new Lazy<SingletonDataContainer>(() => new SingletonDataContainer());
+            }
+            return _instance.Value;
+        }
+
+        public string GetCustomerName(string name) => name;
+    }
+}
